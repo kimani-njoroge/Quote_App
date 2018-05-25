384688713b280ffeb694b4527d918e34ac4e5d33
kimani-njoroge <kimani064@gmail.com>
2018-05-25 11:50:43 +0300

    generate quote-info component to display details of the quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteInfoComponent } from './quote-info/quote-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteInfoComponent
   ],
   imports: [
     BrowserModule
