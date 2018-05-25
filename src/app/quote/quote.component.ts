9b7f0746766db518ae5ff040af1a8b90e4a355b2
kimani-njoroge <kimani064@gmail.com>
2018-05-25 11:41:23 +0300

    generate folder for quote to host our components

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote(1,'quote1'),
+    new Quote(2,'quote2'),
+    new Quote(3,'quote3'),
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
