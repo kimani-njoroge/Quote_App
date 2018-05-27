36f0bc82ed8ef92d030c32c628f07ecd1b8c62ec
kimani-njoroge <kimani064@gmail.com>
2018-05-27 19:54:41 +0300

    style quote app

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,13 +8,13 @@ import { Quote } from '../quote'
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = new Quote(0,"","","");
+  newQuote = new Quote(0,"","","",0,0,"");
 
   @Output() addQuote=new EventEmitter<Quote>();
 
   submitQuote(){
     this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote(0,"","","");
+    this.newQuote = new Quote(0,"","","",0,0,"");
 
   }
 
